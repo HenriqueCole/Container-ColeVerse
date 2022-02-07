ca4beffabb25064fc48ee89dd21d44bc982728cf
HenriqueCole <henrique_c_fernandes@estudante.sesisenai.org.br>
2022-02-07 15:29:46 -0300

    upload de imagemm

--- ColeVerse/src/app/add-and-sell/listar-nft/lista-sua-nft/lista-sua-nft.component.ts
+++ ColeVerse/src/app/add-and-sell/listar-nft/lista-sua-nft/lista-sua-nft.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-var lista = document.querySelector('ul')
 
 
 @Component({
@@ -25,21 +24,28 @@ export class ListaSuaNFTComponent implements OnInit {
     const file = new FileReader
     file.onload = (e) => {
       this.imageURL = e.target.result;
-      var item = document.createElement('li');
-      var image = document.createElement('img');
-      image.src = this.imageURL;
-      console.log(item)
-      item.appendChild(image);
-      lista.appendChild(item);
     }
     this.teste = 1
     file.readAsDataURL(event.target.files[0])
   }
 
 
-  
   abrirModal(){
     this.openModal = 1
+  }
+
+  clickBotao(){
+    var item = document.createElement('li');
+    var image = document.createElement('img');
+    image.src = this.imageURL;
+    console.log(item)
+    item.appendChild(image);
+    document.getElementById("SavedNFT").appendChild(item);
+    image.className = 'imagemSalva'
+
+
+
+    this.openModal = 2;
   }
   
 }
