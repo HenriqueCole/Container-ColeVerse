fc7bd138a5d7cfca0f167d84bd4b74b4811e238e
HenriqueCole <henrique_c_fernandes@estudante.sesisenai.org.br>
2022-02-09 19:45:43 -0300

    remover img from upload

--- ColeVerse/src/app/add-and-sell/listar-nft/lista-sua-nft/lista-sua-nft.component.ts
+++ ColeVerse/src/app/add-and-sell/listar-nft/lista-sua-nft/lista-sua-nft.component.ts
@@ -24,6 +24,10 @@ export class ListaSuaNFTComponent implements OnInit {
     const file = new FileReader
     file.onload = (e) => {
       this.imageURL = e.target.result;
+      setTimeout(( )=>{
+        document.querySelector('#imgNFT').remove()
+        this.imageURL = ""
+      }, 3000)
     }
     this.teste = 1
     file.readAsDataURL(event.target.files[0])
@@ -35,17 +39,25 @@ export class ListaSuaNFTComponent implements OnInit {
   }
 
   clickBotao(){
+    var containerIMG = document.createElement('div')
     var item = document.createElement('li');
     var image = document.createElement('img');
     image.src = this.imageURL;
     console.log(item)
-    item.appendChild(image);
-    document.getElementById("SavedNFT").appendChild(item);
+    containerIMG.appendChild(image);
+    item.appendChild(containerIMG)
+    document.getElementById("SavedNFT").appendChild(containerIMG);
     image.id = 'imagemSalva'
-
-
-
     this.openModal = 2;
+
+    image.style.height = '350px';
+    image.style.width = '350px';
+    containerIMG.id = 'containerIMG'
+    containerIMG.style.backgroundColor = 'blue';
+    containerIMG.style.display = 'flex';
+    containerIMG.style.justifyContent = 'center';
+    containerIMG.style.alignItems = 'center';
   }
+
   
 }
