5ea47569c0968c9bae5fa9c9a98a972e5db289a4
Henrique Cole <henriquecolefernandes@gmail.com>
2022-04-02 23:00:00 -0300

    front finished

--- ColeVerse/src/app/add-and-sell/buy-nft/buy/buy.component.ts
+++ ColeVerse/src/app/add-and-sell/buy-nft/buy/buy.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import swal from 'sweetalert';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -14,8 +15,67 @@ export class BuyComponent implements OnInit {
     private usuarioService: UsuarioService
   ) { }
 
+  idPessoa = localStorage.getItem('IDUSER');
+
+  imageURL
+  teste = 0;
+  openModal;
+  listaNFT = []
+
+  
+  images = [
+
+  ]
+  
   ngOnInit() {
+    console.log(this.idPessoa);
+    fetch('/api/buscar_nft',
+    {
+      method: 'POST', headers: { 'Content-Type': 'application/json' }
+    }).then(function (result) {
+      return result.json();
+    }).then((dados) => {
+      console.log(dados)
+      this.listaNFT = dados.list;
+      console.log("essa é a lista nft --> " , this.listaNFT)
+    }
+    ).catch(function (erro) { console.log(erro); })
+    
+    fetch('/api/inserir_nft',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          nome: this.name,
+          image: this.imageURL,
+          price: this.price
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(function (result) {
+      return result.json();
+    }).then(function (dados) {
+      console.log(dados);
+    }).catch(function (erro) {
+      console.log(erro);
+    })
   }
+
+  
+
+  preco: String = "";
+  nome: String = "";
+  saidaDosInputs = 0;
+
+  mudanca(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.img64 = reader.result;    
+    };
+  }
+  
 
   voltar(){
     this.router.navigate(['/telaAddAndSell'])
